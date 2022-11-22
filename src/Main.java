4bee32378505abc7112c598d28612a94d1651e32
DavidBaldsiefen <david.baldsiefen@gmx.de>
2022-11-22 22:42:53 +0100

    Add framework for output file creation

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,46 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        List<String> lines = FileReader.readOldFormat("files/example_notes_old.txt");
-        List<List<String>> commentStrings = FileReader.splitCommentsOldFormat(lines);
-        for(int i = 0; i < commentStrings.size(); i++)
-        {
-            BookComment bc = BookComment.createFromOldFormat(commentStrings.get(i));
-            System.out.print(bc.toString());
-            System.out.println("------");
-        }
-        for(int i = 0; i < commentStrings.size(); i++)
-        {
-            BookComment bc = BookComment.createFromOldFormat(commentStrings.get(i));
-            List<String> commentsHTML = bc.toNewFormat();
-            for(int j = 0; j < commentsHTML.size(); j++)
-            {
-                System.out.println(commentsHTML.get(j));
-            }
-        }
+        create_pocketbook_files("files/example_notes_old.txt");
     }
 
+    public static void create_pocketbook_files(String oldFormatFile) {
 
+        // read and decode the old file
+        List<String> lines = FileReader.readOldFormat("files/example_notes_old.txt");
+        List<List<String>> commentStrings = FileReader.splitCommentsOldFormat(lines);
+        List<BookComment> bookComments = new ArrayList<>();
+        for(List<String> commentString : commentStrings)
+        {
+            bookComments.add(BookComment.createFromOldFormat(commentString));
+        }
 
+        // create comment lists on a per-book-basis
+        List<String> bookTitles = new ArrayList<>();
+        for(BookComment bc : bookComments)
+        {
+            String author_title = bc.bookAuthor + "_" + bc.bookTitle;
+            if(!bookTitles.contains(author_title))
+            {
+                bookTitles.add(author_title);
+            }
+        }
+        for(String bookTitle : bookTitles)
+        {
+            List<BookComment> booksComments = new ArrayList<>();
+            for(BookComment bc : bookComments)
+            {
+                if((bc.bookAuthor + "_" + bc.bookTitle) == bookTitle)
+                {
+                    booksComments.add(bc);
+                }
+            }
+            // now the list contains all comments of the book. Therefore, we can create the output file
+            // TODO
+        }
+
+    }
 }
